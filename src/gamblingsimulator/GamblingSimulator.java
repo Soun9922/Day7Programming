81aceafeed4c50919382597965620181c32c13d0
Soun9922 <sagsounak39@gmail.com>
2022-05-25 18:32:55 +0530

    Will the Win or Loose code is added

--- src/gamblingsimulator/GamblingSimulator.java
+++ src/gamblingsimulator/GamblingSimulator.java
@@ -1,13 +1,24 @@
 package gamblingsimulator;
 
 public class GamblingSimulator {
-    public void Gambler(){
-        int stakeMoney = 100;
+    final static int STAKE_MONEY = 100;
+
+    public boolean winOrloss() {
+        if (Math.random() < 0.5)
+            return true;
+        else
+            return false;
     }
+
     public static void main(String[] args) {
-        int betPrice = 1;
+        GamblingSimulator gambler = new GamblingSimulator();
+        int betPrice = STAKE_MONEY - 1;
         for (betPrice = 1; betPrice <= 100; betPrice++) {
             System.out.println("You have started the Game with $1");
+            if (gambler.winOrloss()){
+                System.out.println("You have Win the game");
+            }else
+                System.out.println("You Loose the game");
         }
     }
 }
