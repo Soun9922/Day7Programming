9da2da86d907219c2579f9be25b7b42cc2bb298e
Soun9922 <sagsounak39@gmail.com>
2022-05-25 18:11:00 +0530

    The Start of Gambling Simulation code is added

--- src/gamblingsimulator/GamblingSimulator.java
+++ src/gamblingsimulator/GamblingSimulator.java
@@ -1,11 +1,9 @@
 package gamblingsimulator;
 
 public class GamblingSimulator {
-    public void Gambler(){
-        int stakeMoney = 100;
-    }
+        final static int STAKE_MONEY = 100;
     public static void main(String[] args) {
-        int betPrice = 1;
+        int betPrice = STAKE_MONEY - 1;
         for (betPrice = 1; betPrice <= 100; betPrice++) {
             System.out.println("You have started the Game with $1");
         }
