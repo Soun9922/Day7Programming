6df2f279fd3baaacbadd9360a259193f20747f36
Soun9922 <sagsounak39@gmail.com>
2022-05-25 19:23:30 +0530

    Calculative Gambling code is added

--- src/gamblingsimulator/GamblingSimulator.java
+++ src/gamblingsimulator/GamblingSimulator.java
@@ -1,8 +1,6 @@
 package gamblingsimulator;
 
 public class GamblingSimulator {
-    final static int STAKE_MONEY = 100;
-
     public boolean winOrloss() {
         if (Math.random() < 0.5)
             return true;
@@ -12,13 +10,22 @@ public class GamblingSimulator {
 
     public static void main(String[] args) {
         GamblingSimulator gambler = new GamblingSimulator();
-        int betPrice = STAKE_MONEY - 1;
+        int stakeMoney =100;
+        int betPrice = 1;
         for (betPrice = 1; betPrice <= 100; betPrice++) {
             System.out.println("You have started the Game with $1");
-            if (gambler.winOrloss()){
+            if (gambler.winOrloss()) {
+                betPrice++;
                 System.out.println("You have Win the game");
-            }else
+                System.out.println("You have won : "+betPrice);
+                if (betPrice == 50){
+                    System.out.println("You will not playing for the rest of the day.");
+                }
+            } else {
+                betPrice--;
                 System.out.println("You Loose the game");
+                System.out.println("You have loose : "+betPrice);
+            }
         }
     }
 }
